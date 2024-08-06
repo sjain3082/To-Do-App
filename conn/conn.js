bfc45328b2994e4344b6834d5cab47176e6d801b
Shreyansh <sj3082002@gmail.com>
2024-08-06 22:05:25 +0530

    MongoDb COnnection String

--- conn/conn.js
+++ conn/conn.js
@@ -4,7 +4,7 @@ const conn = async (req, res) => {
   try {
     await mongoose
       .connect(
-        ""
+        "mongodb+srv://sj3082002:jgWAwyMIMMGPqOBa@cluster0.oqfqc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
       )
       .then(() => {
         console.log("Connected");
